20caee04b4b945eb5d21db8666da7ba4d23fb2ab
hosuk <kirklayer@gmail.com>
2021-08-05 16:51:53 +0900

    Updates

---
+++ precache-manifest.7b97f5a454fc46102d6e83b1b66905b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1752c59099c306a6197355918a1c86",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4f2099bd6afa394ed693",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "4deeab4236d52bca0bd4",
+    "url": "./static/js/2.bad6dc1a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.bad6dc1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f2099bd6afa394ed693",
+    "url": "./static/js/main.44a63a9b.chunk.js"
+  },
+  {
+    "revision": "35bcc6b6711fa6b1cea1",
+    "url": "./static/js/runtime-main.d242fa34.js"
+  }
+]);
