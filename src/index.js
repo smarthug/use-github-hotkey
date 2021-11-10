c466216d71ee03558c3c7e87456162cc48597bed
hosuk <kirklayer@gmail.com>
2021-11-10 16:52:11 +0900

    migrate to v1.6.0

--- src/index.js
+++ src/index.js
@@ -25,12 +25,39 @@ export function useHotkey(shortcut = null) {
 }
 
 
+function mockGetAttribute(attributeName) {
+  // it look up the data-key-scope attribute
+  // this ... 이걸 호출한 , dom 에서 해야하는가 그것이 가능한가 ??
+  return null
+}
+
+function mockDispatchEvent() {
+
+  return true
+}
+
 export function installFuncHotkey(func = () => { }, shortcut = null) {
 
   const button = {
+    get getAttribute() {
+      return mockGetAttribute
+    },
+    get dispatchEvent() {
+      return mockDispatchEvent
+    },
+
     get click() {
       return func
     }
   }
   install(button, shortcut);
 }
+
+
+// const button = {
+//   getAttribute:()=>(undefined),
+//   dispatchEvent:()=>(true),
+//   get click() {
+//     return func
+//   }
+// }
