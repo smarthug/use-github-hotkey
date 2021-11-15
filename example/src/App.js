429e730b29a24944347a93381c9fc7434aa17781
hosuk <kirklayer@gmail.com>
2021-11-15 14:45:12 +0900

    back to basic

--- example/src/App.js
+++ example/src/App.js
@@ -1,16 +1,16 @@
-import React, {useRef} from 'react'
+import React from 'react'
 import { useHotkey, installFuncHotkey } from 'usehotkey'
 
 export default function App() {
   // keyboard shortcut with dom
-  // const setTestShortcut = useHotkey("Control+d")
+  const setTestShortcut = useHotkey("t e")
   // const setTestShortcut2 = useHotkey("t a")
 
   // keyboard shortcut with function
   installFuncHotkey(handleTTClick, "t t")
 
   // installFuncHotkey(handleForm,"Control+d")
-  installFuncHotkey(handleForm,"Control+d", "text-area-1")
+  // installFuncHotkey(handleForm,"Control+d", "text-area-1")
 
   function handleClick() {
     alert("t e clicked");
@@ -20,17 +20,16 @@ export default function App() {
     alert("t t clicked");
   }
 
-  function handleForm(){
-    alert("form worked")
-  }
+  // function handleForm(){
+  //   alert("form worked")
+  // }
 
   return (
     <div>
       <h1>example</h1>
-      {/* <button data-hotkey-scope="text-area-2"   ref={setTestShortcut} onClick={handleClick} >press "t e" to click this button with hotkey</button> */}
+      <button ref={setTestShortcut} onClick={handleClick} >press "t e" to click this button with hotkey</button>
       <h3>press "t t" to invoke function with hotkey</h3>
-      <textarea id="text-area-1"  rows="4" cols="40"></textarea>
-      <textarea  id="text-area-2"  rows="4" cols="40"></textarea>
+
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
 import { useEffect, useState } from 'react'
 import { install, uninstall } from "@github/hotkey"
-import { doc } from 'prettier'
 
 // export const shortcutMappingObj = {
 //   showCommandPalette: "Control+P",
 // };
-console.log("wtf")
+
 const buttonProto = {
   scope: null,
+  returnNull: () => null,
+  returnScope: () => this.scope,
+  returnTrue: () => true,
   get getAttribute() {
     console.log(this.scope)
     if (this.scope === null) {
 
-      return () => null
+      return this.returnNull
     } else {
-
-      // return () => this.scope
-      console.log(this.scope)
-      return () => this.scope
+      return this.returnScope
     }
   },
   get dispatchEvent() {
-    return mockDispatchEvent
+    return this.returnTrue
   }
 }
 
@@ -50,19 +49,20 @@ function mockDispatchEvent() {
 }
 
 // data-key-scope 를 하나 더 넣어줄까 인자로 ???
-export function installFuncHotkey(func = () => { }, shortcut = null, scope = null) {
+// 확장 , 다른걸로 ... 
+// export function installFuncHotkey(func = () => { }, shortcut = null, scope = null) {
 
-  // 아예 가상의 dom 을 만들어볼까 ???
+//   // 아예 가상의 dom 을 만들어볼까 ???
 
-  const ghost = document.createElement('div')
-  document.querySelector("#root").appendChild(ghost)
-  ghost.setAttribute("data-hotkey-scope", scope)
-  ghost.style.display = "none"
-  const button = Object.create(buttonProto)
-  button.scope = scope
-  Object.defineProperty(button, 'click', { get: () => { return func } })
-  install(button, shortcut);
-}
+//   const ghost = document.createElement('div')
+//   document.querySelector("#root").appendChild(ghost)
+//   ghost.setAttribute("data-hotkey-scope", scope)
+//   ghost.style.display = "none"
+//   const button = Object.create(buttonProto)
+//   button.scope = scope
+//   Object.defineProperty(button, 'click', { get: () => { return func } })
+//   install(button, shortcut);
+// }
 
 
 // const button = {
@@ -72,3 +72,14 @@ export function installFuncHotkey(func = () => { }, shortcut = null, scope = nul
 //     return func
 //   }
 // }
+
+
+
+export function installFuncHotkey(func = () => { }, shortcut = null, scope = null) {
+
+
+  const button = Object.create(buttonProto)
+  button.scope = scope
+  Object.defineProperty(button, 'click', { get: () => { return func } })
+  install(button, shortcut);
+}
